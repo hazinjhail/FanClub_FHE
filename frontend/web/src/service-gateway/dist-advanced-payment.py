fdc9304110960331f57fdbae97cd2f39f9c36b25
hazinjhail <hazinjhail@users.noreply.github.com>
2025-11-16 14:12:50 +0000

    Add dist-advanced-payment.py, resolver-configuration.txt, static-configuration.sol

---
+++ frontend/web/src/service-gateway/dist-advanced-payment.py
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/fanclub_fhe/frontend/web/src/service-gateway/dist-advanced-payment.py
+// Generated at: 2025-12-03T23:45:45.846Z
+
+
